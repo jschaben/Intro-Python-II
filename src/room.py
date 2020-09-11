2e75ed31b46a4778d6e478f6a0635cb799742b68
Josh schaben <jschaben42@gmail.com>
2020-09-10 19:23:21 -0500

    fixed bug

--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def print_name(self):
+        print(f'{self.name}')
+
+    def print_description(self):
+        print(f'{self.description}')
+
+    def outside_routes(self):
+        return()
